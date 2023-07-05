11fe5233e38840cd999b178bc4989e5bebde7323
Winston430 <137290674+Winston430@users.noreply.github.com>
2023-07-05 09:06:52 +0300

    Update winston.py

--- winston.py
+++ winston.py
@@ -3,9 +3,9 @@ import streamlit as st
 def body_temperature_check():
     temperature = st.number_input("Enter your body temperature in Celsius:", min_value=0, max_value=42)
     if temperature > 36:
-        st.write("Your body temperature is greater than 36 Celsius.")
+        st.write("FEVER.")
     else:
-        st.write("Your body temperature is normal.")
+        st.write("NO FEVER.")
 
 if __name__ == "__main__":
     body_temperature_check()
