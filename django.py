54a5a06a0fc4e623e74a839a171141fc1a4d531f
Winston430 <137290674+Winston430@users.noreply.github.com>
2023-12-01 12:01:28 +0300

    django.py

---
+++ django.py
@@ -0,0 +1,29 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super(RegistrationForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
+
+# Example usage:
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/accounts/login/')
+    else:
+        form = RegistrationForm()
+    return render(request, 'registration/register.html', {'form': form})
